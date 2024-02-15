be79afa9a390e8aa1ccdd51ea1d675fc1e7b7144
jacobglanz <jacobglanz9@gmail.com>
2024-02-14 19:49:14 -0500

    Added search by IngredientName param

--- Database Files/3 sprocs/IngredientGet.sql
+++ Database Files/3 sprocs/IngredientGet.sql
@@ -1,5 +1,6 @@
 create or alter procedure dbo.IngredientGet(
     @IngredientId int = 0,
+    @IngredientName varchar(30) = '',
     @All int = 0,
     @IncludeBlank bit = 0)
 as
@@ -8,6 +9,7 @@ begin
     from Ingredient i
     where i.IngredientId = @IngredientId
     or @All = 1
+    or IngredientName like '%' + @IngredientName + '%'
     union select 0, ' '
     where @IncludeBlank = 1
     order by i.IngredientId
@@ -22,4 +24,6 @@ exec IngredientGet @all = 1, @IncludeBlank = 1
 declare @IngredientId int
 select top 1 @IngredientId = i.IngredientId from Ingredient i
 exec IngredientGet @IngredientId = @IngredientId, @IncludeBlank = 1
+
+exec IngredientGet @IngredientName = 'bak'
 */
